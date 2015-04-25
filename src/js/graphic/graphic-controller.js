8735b56fea664206a41c651f76bf4e1a18af42f3
Matthew Bednarski <matthew.bednarski@ekr.it>
2015-04-25 22:44:54 +0200

    updated d3 service and directive

--- src/js/graphic/graphic-controller.js
+++ src/js/graphic/graphic-controller.js
@@ -16,6 +16,142 @@ app.factory('d3Svc', [
 
     function() {
         var _d3 = d3;
+        var width = 960,
+            height = 450,
+            radius = Math.min(width, height) / 2;
+
+        _d3.setup = function(ele) {
+            var svg = d3.select(ele)
+                .append("svg")
+                .attr("height", height)
+                .attr("width", width)
+                .append("g");
+
+            svg.append("g")
+                .attr("class", "slices");
+            svg.append("g")
+                .attr("class", "labels");
+            svg.append("g")
+                .attr("class", "lines");
+        	svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        	return svg;
+        };
+
+        var pie = d3.layout.pie()
+            .sort(function(d) {
+                return d.label;
+            })
+            .value(function(d) {
+                return d.price;
+            });
+
+        var arc = d3.svg.arc()
+            .outerRadius(radius * 0.95)
+            .innerRadius(radius * 0.3);
+
+        var outerArc = d3.svg.arc()
+            .innerRadius(radius * 0.9)
+            .outerRadius(radius * 0.9);
+
+        var key = function(d) {
+            return d.data.label;
+        };
+
+        var color = d3.scale.category20();
+
+        _d3.change = function(data, svg) {
+
+            console.log(data);
+            /* ------- PIE SLICES -------*/
+            var slice = svg.select(".slices").selectAll("path.slice")
+                .data(pie(data), key);
+
+            slice.enter()
+                .insert("path")
+                .style("fill", function(d) {
+                    return color(d.data.label);
+                })
+                .attr("class", "slice");
+
+            slice
+                .transition().duration(1000)
+                .attrTween("d", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        return arc(interpolate(t));
+                    };
+                });
+
+            slice.exit()
+                .remove();
+
+            /* ------- TEXT LABELS -------*/
+
+            var text = svg.select(".labels").selectAll("text")
+                .data(pie(data), key);
+
+            text.enter()
+                .append("text")
+                .attr("dy", ".35em")
+                .text(function(d) {
+                    return d.data.label;
+                });
+
+            function midAngle(d) {
+                return d.startAngle + (d.endAngle - d.startAngle) / 2;
+            }
+
+            text.transition().duration(1000)
+                .attrTween("transform", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        var pos = outerArc.centroid(d2);
+                        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+                        return "translate(" + pos + ")";
+                    };
+                })
+                .styleTween("text-anchor", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        return midAngle(d2) < Math.PI ? "start" : "end";
+                    };
+                });
+
+            text.exit()
+                .remove();
+
+            /* ------- SLICE TO TEXT POLYLINES -------*/
+
+            var polyline = svg.select(".lines").selectAll("polyline")
+                .data(pie(data), key);
+
+            polyline.enter()
+                .append("polyline");
+
+            polyline.transition().duration(1000)
+                .attrTween("points", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        var pos = outerArc.centroid(d2);
+                        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                        return [arc.centroid(d2), outerArc.centroid(d2), pos];
+                    };
+                });
+
+            polyline.exit()
+                .remove();
+        }
         return _d3;
     }
 ]);
@@ -29,57 +165,17 @@ app.directive('d3Bars', ['d3Svc', 'itemsSvc',
             },
             link: function(scope, ele, attrs) {
                 // console.log(d3);
-
                 scope.data = scope.$eval(scope.items);
                 console.log(scope.data);
-
-
-                var width = 960,
-                    height = 500;
-
-                var svg = d3.select(ele[0])
-                    .append("svg")
-                    .attr("height", height)
-                    .attr("width", width);
-                var g = svg
-                    .append("g")
-                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-                var color = d3.scale.category20();
-
-                var min = Math.min(width, height);
-                var oRadius = min / 2 * 0.9;
-                var iRadius = min / 2 * 0.85;
-                // .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-                var pie = d3.layout.pie()
-                    .sort(null)
-                    .value(function(d) {
-                        return d.price;
-                    });
-
-                var arc = d3.svg.arc()
-                    .outerRadius(oRadius)
-                    .innerRadius(iRadius);
-
-                var path = g.datum(scope.data)
-                    .selectAll('path')
-                    .data(pie)
-                    .enter()
-                    .append('path')
-                    .attr("class", "piechart")
-                    .attr("fill", function(d, i) {
-                        return color(i);
-                    })
-                    .attr("d", arc)
-                    .each(function(d) {
-                        this._current = d
-                    });
+                var svg = d3.setup(ele[0]);
+                d3.change(scope.data, svg);
 
                 scope.$watch(function() {
                     return scope.items;
                 }, function(nItems) {
                     scope.data = scope.$eval(nItems);
                     console.log(scope.data);
+                    d3.change(scope.data, svg);
                 });
             }
         }
